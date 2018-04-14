17d5a1f854f6d64ecfc5f16379bc3bab339eb413
Senal Wijeratne <senal.ra128@gmail.com>
2018-04-14 23:09:30 +0530

    "Add Developers" Page

--- config/routes.js
+++ config/routes.js
@@ -35,6 +35,7 @@ module.exports.routes = {
   'GET /account/profile':    { action: 'account/view-edit-profile' },
 
   'GET /developers':         { action: 'developers/view-developers' },
+  'GET /developers/add-developers':         { action: 'developers/add-developers' },
 
 
   //  ╔═╗╔═╗╦  ╔═╗╔╗╔╔╦╗╔═╗╔═╗╦╔╗╔╔╦╗╔═╗
