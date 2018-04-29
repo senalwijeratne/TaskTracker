c84928c712e1296d05621437df392d3819690bd8
Senal Wijeratne <senal.ra128@gmail.com>
2018-04-29 11:04:55 +0530

    action now returns projSummary

--- api/controllers/dashboard/view-welcome.js
+++ api/controllers/dashboard/view-welcome.js
@@ -29,6 +29,7 @@ module.exports = {
     })
     .populate('manager')
     .populate('dev')
+    .populate('tasks')
     .intercept((err)=>{
        err.message = 'Something went wrong somewhere, contact tech support : '+ err.message;
        return err;
@@ -36,41 +37,87 @@ module.exports = {
     // sails.log('projects are :',projects)
 
 
-    await projects.forEach( project => {
+    // projects.forEach( project => {
+    //
+    //   project.dev.forEach(async (dev) => {
+    //
+    //     let allTasksOnThisProject = await Task.find({
+    //       where: {
+    //         project: project.id,
+    //         assignedDev: dev.id,
+    //       }
+    //     })
+    //
+    //     let totalHours = 0
+    //     let totalOvetime = 0
+    //
+    //     allTasksOnThisProject.forEach( task => {
+    //
+    //       totalHours += task.taskHours
+    //       totalOvetime += task.taskOvertime
+    //
+    //     })
+    //
+    //     dev.totalHours = totalHours
+    //     dev.totalOvetime = totalOvetime
+    //
+    //     console.log('~~~~~~~~~Two keys added:', dev);
+    //   })
+    //
+    // })
+    //
+    // console.log('This is what the returned projects[0]-dev look like: ', projects[0].dev);
 
-      project.dev.forEach(async (dev) => {
 
-        let allTasksOnThisProject = await Task.find({
-          where: {
-            project: project.id,
-            assignedDev: dev.id,
+
+    projects.forEach( project => {
+
+      let projSummary = {}
+
+      projSummary.projectID = project.id
+      projSummary.devDetails = []
+
+      let projectDevelopers = project.dev
+      let totalProjectHoursWorked = 0
+
+      projectDevelopers.forEach( dev =>{
+        let devSummary = {}
+        let individualWorkHours = 0
+        let individualOvertimeHours = 0
+
+        project.tasks.forEach( task => {
+
+          if(task.assignedDev === dev.id){
+            individualWorkHours += task.taskHours
+            individualOvertimeHours += task.taskOvertime
+
+            totalProjectHoursWorked += task.taskHours + task.taskOvertime
           }
         })
 
-        console.log('');
-        console.log('-------------For Project: ', project.projectName);
-        console.log('-------------For Dev: ', dev.fullName);
-        console.log('allTasksOnThisProject are:', allTasksOnThisProject);
+          devSummary.id = dev.id
+          devSummary.fullName = dev.fullName
+          devSummary.workHours = individualWorkHours
+          devSummary.overTimeHours = individualOvertimeHours
 
-        let totalHours = 0
-        let totalOvetime = 0
-
-        await allTasksOnThisProject.forEach( task => {
-
-          totalHours += task.taskHours
-          totalOvetime += task.taskOvertime
+          projSummary.devDetails.push(devSummary)
 
         })
 
-        dev.totalHours = totalHours
-        dev.totalOvetime = totalOvetime
+        projSummary.totalProjectHours = totalProjectHoursWorked
 
-        console.log('~~~~~~~~~Two keys added:', dev);
+        projSummary.devDetails.forEach( dev => {
+          let {workHours, overTimeHours} = dev
+          let {totalProjectHours} = projSummary
+
+          dev.contribution = Math.round((( workHours + overTimeHours )/totalProjectHours)*100)
+        })
+
+        project.projSummary = projSummary
+
       })
 
-    })
-
-    console.log('~~~~~~~~~~~~~~~~~~~~~~This is what projects[0]-dev look like: ', projects[0].dev);
+    sails.log('example projectSummary for index 0 :',projects[0].projSummary)
 
     return exits.success({
         projects: projects,
